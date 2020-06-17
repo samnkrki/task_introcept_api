054ac89ecefa37a4902666aa224042aab25db117
samnkrki <samn_krki@live.com>
2020-06-17 09:36:53 +0545

    changed code because package didnot save header when append was true

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { CsvHeader, CsvBody } from './interface/csvInterface';
+import { resolve } from 'path';
 const fs = require('fs');
 const csv = require('csv-parser')
 const createCsvWriter = require('csv-writer').createObjectCsvWriter;
@@ -14,11 +15,12 @@ export class AppService {
           // console.log('error', err)
           reject({ success: false, message: "Error reading csv file", err })
         })
-        .pipe(csv())
+        .pipe(csv({ columns: true }))
         .on('data', (row) => {
           userDetails.push(row)
         })
         .on('end', () => {
+          console.log(userDetails)
           resolve({ success: true, data: userDetails })
         })
     })
@@ -26,30 +28,51 @@ export class AppService {
 
   saveUserData(userDetail: CsvBody) {
     const header: CsvHeader[] = [
-      { id: 'name', title: 'Name' },
-      { id: 'educationBackground', title: 'Educational Background' },
-      { id: 'email', title: 'Email Address' },
-      { id: 'gender', title: 'Gender' },
-      { id: 'dob', title: 'Date of Birth' },
-      { id: 'nationality', title: 'Nationality' },
-      { id: 'phone', title: 'Phone number' },
-      { id: 'prefModeContact', title: 'Preferred Contact mode' },
-      { id: "address", title: "Address" }
+      { id: 'name', title: 'name' },
+      { id: 'educationBackground', title: 'educationBackground' },
+      { id: 'email', title: 'email' },
+      { id: 'gender', title: 'gender' },
+      { id: 'dob', title: 'dob' },
+      { id: 'nationality', title: 'nationality' },
+      { id: 'phone', title: 'phone' },
+      { id: 'prefModeContact', title: 'prefModeContact' },
+      { id: "address", title: "address" }
     ]
-    const csvWriter = createCsvWriter({
-      path: 'userlist.csv',
-      header,
-      append: true
-    });
-    const data = [userDetail];
+    return new Promise((resolve, reject) => {
+      fs.createReadStream('userlist.csv')
+        .on('data', () => {
+          const csvWriter = createCsvWriter({
+            path: 'userlist.csv',
+            header,
+            append: true
+          });
+          const data = [userDetail];
+          csvWriter
+            .writeRecords(data)
+            .then(() => {
+              resolve({
+                success: true,
+                message: "The CSV file was written successfully"
+              })
+            })
+        })
+        .on('error', () => {
+          const csvWriter = createCsvWriter({
+            path: 'userlist.csv',
+            header,
+          });
+          const data = [userDetail];
+          return csvWriter
+            .writeRecords(data)
+            .then(() => {
+              resolve({
+                success: true,
+                message: "The CSV file was written successfully for the first time"
+              })
+            })
+        })
+    })
 
-    return csvWriter
-      .writeRecords(data)
-      .then(() => {
-        return {
-          success: true,
-          message: "The CSV file was written successfully"
-        }
-      })
+
   }
 }
