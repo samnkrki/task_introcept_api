791af368c2259128f3d949338f83c28e2dcaeb39
samnkrki <samn_krki@live.com>
2020-06-17 08:27:00 +0545

    added interface for csv head and body

---
+++ src/interface/csvInterface.ts
@@ -0,0 +1,16 @@
+export interface CsvHeader {
+    title: string,
+    id: string
+}
+
+export interface CsvBody {
+    name: string,
+    educationBackground: string,
+    email: string,
+    gender: string,
+    dob: string,
+    nationality: string,
+    phone: string,
+    prefModeContact: string,
+    address: string,
+}
