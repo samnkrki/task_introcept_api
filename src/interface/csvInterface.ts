1f672e2442514a6e1237ee2260c61515414b3c17
samnkrki <samn_krki@live.com>
2020-06-18 11:04:40 +0545

    added class validators, added _id field for unique data, changed interface to class to support validators

--- src/interface/csvInterface.ts
+++ src/interface/csvInterface.ts
@@ -1,16 +1,50 @@
+import { IsString, IsNotEmpty, IsEmail, IsPhoneNumber, IsEmpty, IsEnum } from 'class-validator'
+import { Gender, PrefModeContact, Nationality } from '../enums/csvFieldsEnum'
+
 export interface CsvHeader {
     title: string,
     id: string
 }
 
-export interface CsvBody {
-    name: string,
-    educationBackground: string,
-    email: string,
-    gender: string,
-    dob: string,
-    nationality: string,
-    phone: string,
-    prefModeContact: string,
-    address: string,
+export class CsvBody {
+    @IsString()
+    @IsEmpty()
+    _id: string
+
+    @IsString()
+    @IsNotEmpty()
+    name: string
+
+    @IsString()
+    educationBackground: string
+
+    @IsEmail()
+    email: string
+
+    @IsString()
+    @IsEnum(Gender, {
+        message: 'Gender must be either "Male", "Female" or "Others"',
+    })
+    gender: string
+
+    @IsString()
+    dob: string
+
+    @IsString()
+    @IsEnum(Nationality, {
+        message: 'Nationality must either be a "Nepali" or "Others"',
+    })
+    nationality: string
+
+    @IsPhoneNumber("NP")
+    phone: string
+
+    @IsString()
+    @IsEnum(PrefModeContact, {
+        message: 'Preference must either be an "Email", a "Phone" or "None"',
+    })
+    prefModeContact: string
+
+    @IsString()
+    address: string
 }
