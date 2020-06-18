247fba6df263193974c91affd9d09c77abeb8ab0
samnkrki <samn_krki@live.com>
2020-06-18 11:07:51 +0545

    added a fle to contain all the enum fields at one place

---
+++ src/enums/csvFieldsEnum.ts
@@ -0,0 +1,16 @@
+export enum Gender {
+    MALE = 'Male',
+    FEMALE = 'Female',
+    OTHERS = "Others"
+}
+
+export enum Nationality {
+    NEPALI = "Nepali",
+    OTHERS = "Others"
+}
+
+export enum PrefModeContact {
+    EMAIL = 'Email',
+    PHONE = 'Phone',
+    NONE = 'None'
+}
