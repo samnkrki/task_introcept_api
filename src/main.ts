440f363ea5c89470ea1fea3b297e0fb70a05c9e2
samnkrki <samn_krki@live.com>
2020-06-18 11:10:45 +0545

    added pipes to validate data for the api and filters to handle exceptions thrown in the code. Response for exceptions is sent from here.

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidateInputPipe } from './pipes/validate.pipe';
+import { AnyExceptionFilter } from './exceptionFilters/exceptionFilter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
+  app.useGlobalPipes(new ValidateInputPipe())
+  app.useGlobalFilters(new AnyExceptionFilter());
   await app.listen(3001);
 }
 bootstrap();
